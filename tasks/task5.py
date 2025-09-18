04736a17b3a62aee8270504a3c15ec58d1035f0a
kymir4ik <dima-zhiltsov@bk.ru>
2025-09-18 10:02:06 +0500

    сделал дз

--- tasks/task5.py
+++ tasks/task5.py
@@ -4,7 +4,10 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
-
+    numbers = input("Введите числа: ") 
+    numbers_list = list(map(int, numbers.split())) 
+    squares = [x**2 for x in numbers_list]
+    print("Результат:", ' '.join(map(str, squares)))
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
